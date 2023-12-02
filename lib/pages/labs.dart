1f5ef3482c123945f01eabc72087dae738efb1aa
Jewel1311 <jewelmathew13@gmail.com>
2023-12-02 10:22:31 +0530

    design changes

--- lib/pages/labs.dart
+++ lib/pages/labs.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:elab/style/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
@@ -20,7 +22,6 @@ class _LabsState extends State<Labs> {
 
   final TextEditingController searchController = TextEditingController();
 
-
   @override
   void initState() {
     super.initState();
@@ -33,28 +34,25 @@ class _LabsState extends State<Labs> {
     searchController.dispose();
   }
 
-
   String convert24HourTo12Hour(String time24Hour) {
     final inputFormat = DateFormat('HH:mm:ss');
     final outputFormat = DateFormat('h:mm a');
     final dateTime = inputFormat.parse(time24Hour);
     final time12Hour = outputFormat.format(dateTime);
     return time12Hour;
-}
+  }
 
-
-
-  Future getLabs() async{
+  Future getLabs() async {
     setState(() {
       isLoading = true;
     });
 
     if (searchController.text == '') {
       final uid = supabase.auth.currentUser!.id;
-      final data = await supabase.from('profile').select().match({'user_id':uid});
+      final data =
+          await supabase.from('profile').select().match({'user_id': uid});
       city = data[0]['city'];
-    }
-    else{
+    } else {
       city = searchController.text.trim();
     }
     labs = await supabase.from('labs').select().ilike('city', '%$city%');
@@ -63,109 +61,189 @@ class _LabsState extends State<Labs> {
       isLoading = false;
     });
   }
-  
+
+  Color getRandomColor() {
+  final List<Color> colorSet = [
+    ElabColors.primaryColor.withOpacity(0.5),
+    Color.fromARGB(255, 235, 16, 16).withOpacity(0.5),
+    Color.fromARGB(255, 93, 235, 16).withOpacity(0.5),
+    Color.fromARGB(255, 16, 235, 213).withOpacity(0.5),
+    Color.fromARGB(255, 203, 209, 13).withOpacity(0.5),
+    Color.fromARGB(255, 154, 12, 186).withOpacity(0.5),  
+  ];
+
+  final random = Random();
+  return colorSet[random.nextInt(colorSet.length)];
+}
+
   @override
   Widget build(BuildContext context) {
-
-
     return Container(
       color: Colors.white,
       child: Padding(
         padding: const EdgeInsets.all(0.0),
-        child: isLoading? const SpinKitFadingCircle(color: ElabColors.primaryColor,) :
-        Column(
-          children: [
-            searchBox(),
-            labs.length> 0 ? labList():noLabsFound(),
-          ],
-        ),
+        child: isLoading
+            ? const SpinKitFadingCircle(
+                color: ElabColors.primaryColor,
+              )
+            : Column(
+                children: [
+                  searchBox(),
+                  labs.length > 0 ? labList() : noLabsFound(),
+                ],
+              ),
       ),
     );
   }
 
   Padding noLabsFound() {
     return const Padding(
-          padding:  EdgeInsets.all(20.0),
-          child: Center(child: Text("No Labs found in your area !", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),),
-        );
+      padding: EdgeInsets.all(20.0),
+      child: Center(
+        child: Text(
+          "No Labs found in your area !",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
   }
-  
 
   Expanded labList() {
-    return Expanded(child: ListView.builder(
-          itemCount: labs.length,
-          itemBuilder: (context, index){
-            return Container(
-              margin: const EdgeInsets.fromLTRB(10,8,10,5),
-              decoration: const BoxDecoration(
-                  color: Colors.white,
-                  border: Border(
-                    top: BorderSide(
-                      color: ElabColors.color3,
-                      width: 1.0
-                    )
-                  )
-                ),
-                child: GestureDetector (onTap:(){
-                   Navigator.pushNamed(context, '/tests', arguments: {'labId' :labs[index]['id'], 'labName':labs[index]['labname'], 'opentime':labs[index]['opentime'], 'closetime':labs[index]['closetime']});
-                } , child: ListTile(
-                  contentPadding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-                  title: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          const Icon(Icons.biotech_rounded, color: ElabColors.secondaryColor, size: 30,),
-                          Text(labs[index]['labname'], 
-                          style: const TextStyle(fontWeight: FontWeight.bold,fontSize: 18),
+    return Expanded(
+        child: ListView.builder(
+            itemCount: labs.length,
+            itemBuilder: (context, index) {
+              return Container(
+                  margin: const EdgeInsets.fromLTRB(10, 8, 10, 5),
+                  decoration: const BoxDecoration(
+                      color: Colors.white,
+                      border: Border(
+                          top: BorderSide(
+                              color: ElabColors.color3, width: 1.0))),
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.pushNamed(context, '/tests', arguments: {
+                        'labId': labs[index]['id'],
+                        'labName': labs[index]['labname'],
+                        'opentime': labs[index]['opentime'],
+                        'closetime': labs[index]['closetime']
+                      });
+                    },
+                    child: ListTile(
+                        contentPadding: const EdgeInsets.symmetric(
+                            vertical: 10, horizontal: 5),
+                        title: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Container(
+                                  decoration: BoxDecoration(
+                                    color:getRandomColor(),
+                                    borderRadius: BorderRadius.circular(
+                                        50.0), // Set the border radius to make it rounded
+                                  ),
+                                  padding: EdgeInsets.all(
+                                      10.0), // Add padding to the container if needed
+                                  child: Icon(
+                                    Icons.biotech_rounded,
+                                    color: Colors.black,
+                                    size: 30,
+                                  ),
+                                ),
+                                const SizedBox(width:8,),
+                                Flexible(child: Text(
+                                  labs[index]['labname'],
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18),
+                                ),
+                                )
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(5, 0, 0, 0),
+                              child: Row(
+                                children: [
+                                  const Icon(
+                                    Icons.location_on_outlined,
+                                    size: 18,
+                                    color: ElabColors.secondaryColor,
+                                  ),
+                                  Text(labs[index]['city']),
+                                ],
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(5, 0, 0, 0),
+                              child: Row(
+                                children: [
+                                  const Icon(
+                                    Icons.phone,
+                                    size: 18,
+                                    color: ElabColors.secondaryColor,
+                                  ),
+                                  const SizedBox(
+                                    width: 5,
+                                  ),
+                                  SelectableText(
+                                      labs[index]['phone'].toString()),
+                                  const SizedBox(
+                                    width: 5,
+                                  ),
+                                  const Icon(Icons.access_time,
+                                      size: 18,
+                                      color: ElabColors.secondaryColor),
+                                  const SizedBox(
+                                    width: 5,
+                                  ),
+                                  Text(
+                                    convert24HourTo12Hour(
+                                            labs[index]['opentime']) +
+                                        ' - ' +
+                                        convert24HourTo12Hour(
+                                            labs[index]['closetime']),
+                                    style: const TextStyle(color: Colors.black),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        trailing: Container(
+                          decoration: const BoxDecoration(
+                              color: ElabColors.greyColor2,
+                              shape: BoxShape.rectangle,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10))),
+                          child: Padding(
+                            padding: const EdgeInsets.all(5.0),
+                            child: GestureDetector(
+                              onTap: () {
+                                Navigator.pushNamed(context, '/tests',
+                                    arguments: {
+                                      'labId': labs[index]['id'],
+                                      'labName': labs[index]['labname'],
+                                      'opentime': labs[index]['opentime'],
+                                      'closetime': labs[index]['closetime']
+                                    });
+                              },
+                              child: const Icon(
+                                  Icons.keyboard_arrow_right_outlined,
+                                  size: 25,
+                                  color: Colors.black),
+                            ),
                           ),
-                        ],
-                      ),
-                      const SizedBox(height: 8,),
-                    
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(5,0,0,0),
-                        child: Row(
-                        children: [
-                          const Icon(Icons.phone, size: 20,),
-                          const SizedBox(width: 5,),
-                          SelectableText(labs[index]['phone'].toString())
-                        ],
-                                          ),
-                      ),
-                   const SizedBox(height: 10,),
-                   Padding(
-                     padding: const EdgeInsets.fromLTRB(5,0,0,0),
-                     child: Text(labs[index]['city']+ ' | '+ convert24HourTo12Hour(labs[index]['opentime']) + ' - '+  convert24HourTo12Hour(labs[index]['closetime']),
-                      style: const TextStyle(color: Colors.black),
-                      ),
-                   ),
-                    ],
-                  ),
-                  trailing: Container(
-                    decoration: const BoxDecoration(
-                      color: ElabColors.greyColor2,
-                      shape: BoxShape.rectangle, 
-                      borderRadius: BorderRadius.all(Radius.circular(10))
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(5.0),
-                      child: GestureDetector(
-                        onTap: () {
-                          Navigator.pushNamed(context, '/tests', arguments: {'labId' :labs[index]['id'], 'labName':labs[index]['labname'],'opentime':labs[index]['opentime'], 'closetime':labs[index]['closetime']});
-                        },
-                        child: const Icon(Icons.keyboard_arrow_right_outlined, size: 25, color: Colors.black),
-                      ),
-                    ),
-                  )
-
-                ),
-                )
-            );
-          }
-        )
-    );
+                        )),
+                  ));
+            }));
   }
 
   Column searchBox() {
@@ -174,47 +252,49 @@ class _LabsState extends State<Labs> {
         Padding(
           padding: const EdgeInsets.fromLTRB(15, 10, 15, 10),
           child: TextField(
-
-                controller: searchController,
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: ElabColors.greyColor2,
-                  hintText: 'Search city...',
-                  suffixIcon: GestureDetector(
-                    onTap: () {
-                      if(searchController.text != ''){
-                        getLabs();
-                      }
-                    },
-                    child: const Icon(Icons.search, size: 30,),
-                  ),
-                  border: const OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(10)
-                    ),
-                    borderSide: BorderSide.none
-                  ),
+            controller: searchController,
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: ElabColors.greyColor2,
+              hintText: 'Search city...',
+              suffixIcon: GestureDetector(
+                onTap: () {
+                  if (searchController.text != '') {
+                    getLabs();
+                  }
+                },
+                child: const Icon(
+                  Icons.search,
+                  size: 30,
                 ),
-                style: const TextStyle(fontWeight: FontWeight.w700),
               ),
+              border: const OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  borderSide: BorderSide.none),
+            ),
+            style: const TextStyle(fontWeight: FontWeight.w700),
+          ),
         ),
         Padding(
           padding: const EdgeInsets.fromLTRB(15, 15, 0, 10),
           child: Row(
             children: [
-              Text("Labs near you", style: 
-              TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                fontFamily: GoogleFonts.poppins().fontFamily
+              Text(
+                "Labs near you",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: GoogleFonts.poppins().fontFamily),
               ),
-              ),
-              const Icon(Icons.location_on_outlined, size: 25,color: ElabColors.primaryColor,)
+              const Icon(
+                Icons.location_on_outlined,
+                size: 25,
+                color: ElabColors.primaryColor,
+              )
             ],
           ),
         )
       ],
     );
-   
   }
 }
